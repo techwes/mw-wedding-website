2857bf6b5e8b921fe9f5c4bfa17e3a8b64efa1f6
Wesley Ripley <techwesR@gmail.com>
2023-02-05 16:31:22 -0500

    Publish with basePath updates

--- docs/_next/static/nmDPPnKxYAEs1VD2KRZv2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-0a8b3f0884b9e48e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
